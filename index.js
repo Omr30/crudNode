701e55f0c087511d4b9a5597c3327dcd2b3714cc
mvrk <oscarmenacho3@gmail.com>
2022-11-05 21:38:10 -0500

    Conexión a Mysql

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { pool } from "./db.js";
 
 const app = express();
 const port = 8080;
+
+app.get('/ping', async(req,res) => {
+    const [result] = await pool.query('SELECT "pong" AS result');
+    res.json(result[0])
+})
 
 app.get('/employees', (req, res) => {
     res.send('Obteniendo empleados')
