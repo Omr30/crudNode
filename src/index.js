7ab7429e78d8c4b5614e7191bb65545a3f95382a
mvrk <oscarmenacho3@gmail.com>
2022-11-08 13:34:58 -0500

    Not fount

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ const port = 8080;
 
 app.use(express.json())
 
+// Rutas del endpoint
 app.use(indexRoutes);
 app.use('/api', employeesRoutes);
+
+// Si busca una ruta no existente
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'endpoint not found'
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
